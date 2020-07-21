17899b2146d51f73c9d871759427e0deef0ea4b6
Pedro Cavalcante <pedrocolrj@gmail.com>
2020-07-21 15:36:34 -0300

    teste

--- R/white_noise.R
+++ R/white_noise.R
@@ -2,12 +2,13 @@
 #' @title White Noise
 #'
 #' 
-#' @param .distfn What function should be used to draw the innovations from? Must be a string. 
+#' @param .distfn What function should be used to draw the innovations from? 
+#' Must be a string. 
 #' Defaults to `"rnorm"`, should work with any `r_` compatible function.
 #' @param y0 The initial condition
 #' @param ... arguments to be passed to `.distfn`
-#' @param n sample size, only has to be specified if function is called outside of a tibble or similar data context
-#'
+#' @param n sample size, only has to be specified if function is called 
+#' outside of a tibble or similar data context
 #' 
 #' @import tibble
 #' @import purrr
@@ -32,7 +33,7 @@ white_noise <- function(.distfn = "rnorm", n = NULL, y0 = 0, ...) {
 
   } else if(is.null(n)) {
 
-    abort("n must be a non-negative integer")
+    rlang::abort("n must be a non-negative integer")
 
   }
 
@@ -43,3 +44,5 @@ white_noise <- function(.distfn = "rnorm", n = NULL, y0 = 0, ...) {
                     `+`)
 
 }
+
+
